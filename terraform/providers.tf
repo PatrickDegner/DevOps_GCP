c485237094e40fce827325505b88bb474a6940d8
PatrickDegner <acaer@gmx.net>
2025-02-02 14:06:56 +0100

    destroy

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -24,13 +24,13 @@ provider "google" {
 
 data "google_client_config" "devops_cluster" {}
 
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.devops_cluster.endpoint}"
-  token                  = data.google_client_config.devops_cluster.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.devops_cluster.master_auth[0].cluster_ca_certificate)
+# provider "kubernetes" {
+#   host                   = "https://${google_container_cluster.devops_cluster.endpoint}"
+#   token                  = data.google_client_config.devops_cluster.access_token
+#   cluster_ca_certificate = base64decode(google_container_cluster.devops_cluster.master_auth[0].cluster_ca_certificate)
 
-  ignore_annotations = [
-    "^autopilot\\.gke\\.io\\/.*",
-    "^cloud\\.google\\.com\\/.*"
-  ]
-}
+#   ignore_annotations = [
+#     "^autopilot\\.gke\\.io\\/.*",
+#     "^cloud\\.google\\.com\\/.*"
+#   ]
+# }
