3b716726da3ada23cffa8dc3bf51d5e4a47cd8af
PatrickDegner <acaer@gmx.net>
2025-02-02 08:43:49 +0100

    node count

--- terraform/container.tf
+++ terraform/container.tf
@@ -6,11 +6,10 @@ resource "google_artifact_registry_repository" "devops-repo" {
 }
 
 resource "google_container_cluster" "devops_cluster" {
-  name               = "devops-cluster"
-  location           = var.region
-  initial_node_count = 1
-  network            = google_compute_network.devops_vpc.id
-  subnetwork         = google_compute_subnetwork.devops_subnet.name
+  name       = "devops-cluster"
+  location   = var.region
+  network    = google_compute_network.devops_vpc.id
+  subnetwork = google_compute_subnetwork.devops_subnet.name
 
   node_pool {
     name       = "default-pool"
