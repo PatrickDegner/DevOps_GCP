119429cd1cc689b99c5539c89ac3f27506b45944
PatrickDegner <acaer@gmx.net>
2025-02-02 08:39:31 +0100

    fmt

--- terraform/container.tf
+++ terraform/container.tf
@@ -9,12 +9,12 @@ resource "google_container_cluster" "devops_cluster" {
   name               = "devops-cluster"
   location           = var.region
   initial_node_count = 1
-  network = google_compute_network.devops_vpc.id
-  subnetwork = google_compute_subnetwork.devops_subnet.name
+  network            = google_compute_network.devops_vpc.id
+  subnetwork         = google_compute_subnetwork.devops_subnet.name
 
   node_pool {
-    name         = "default-pool"
-    node_count   = 1
+    name       = "default-pool"
+    node_count = 1
     node_config {
       machine_type = "e2-standard-2"
     }
